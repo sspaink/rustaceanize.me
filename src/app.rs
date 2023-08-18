3f178a1d98f1a2d3682c7e35c80b7f9a949f97b8
Sebastian Spaink <sspaink@influxdata.com>
2023-08-18 02:03:19 -0500

    working version

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,106 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use egui::RichText;
+use egui_extras::RetainedImage;
+use std::collections::HashMap;
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+struct Image {
+    main: RetainedImage,
+    thumb: RetainedImage,
 }
 
-impl Default for TemplateApp {
+struct Body {
+    color: String,
+    hat: Option<String>,
+}
+
+pub struct App {
+    removeThumb: RetainedImage,
+    bodies: HashMap<String, Image>,
+    hats: HashMap<String, Image>,
+    ferris: Body,
+}
+
+impl Default for App {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            removeThumb: RetainedImage::from_image_bytes(
+                "remove",
+                include_bytes!("../assets/remove_thumb.png"),
+            ).unwrap(),
+            bodies: (|| {
+                // TODO: Move to a build script or read from file at startup
+                let mut images: HashMap<String, Image> = HashMap::new();
+
+                let image = RetainedImage::from_image_bytes(
+                    "purple",
+                    include_bytes!("../assets/colors/purple.png"),
+                )
+                .unwrap();
+
+                let thumb = RetainedImage::from_image_bytes(
+                    "orange",
+                    include_bytes!("../assets/colors/purple_thumb.png"),
+                )
+                .unwrap();
+                images.insert(
+                    "purple".to_string(),
+                    Image {
+                        main: image,
+                        thumb: thumb,
+                    },
+                );
+
+                let image = RetainedImage::from_image_bytes(
+                    "orange",
+                    include_bytes!("../assets/colors/orange.png"),
+                )
+                .unwrap();
+
+                let thumb = RetainedImage::from_image_bytes(
+                    "orange",
+                    include_bytes!("../assets/colors/orange_thumb.png"),
+                )
+                .unwrap();
+                images.insert(
+                    "orange".to_string(),
+                    Image {
+                        main: image,
+                        thumb: thumb,
+                    },
+                );
+
+                images
+            })(),
+            hats: (|| {
+                // TODO: Move to a build script or read from file at startup
+                let mut images: HashMap<String, Image> = HashMap::new();
+
+                let image =
+                    RetainedImage::from_image_bytes("top", include_bytes!("../assets/hats/top.png"))
+                        .unwrap();
+                let thumb = RetainedImage::from_image_bytes(
+                    "orange",
+                    include_bytes!("../assets/hats/top_thumb.png"),
+                )
+                .unwrap();
+                images.insert(
+                    "top".to_string(),
+                    Image {
+                        main: image,
+                        thumb: thumb,
+                    },
+                );
+
+                images
+            })(),
+            ferris: Body {
+                color: "orange".to_string(),
+                hat: Some("top".to_string()),
+            },
         }
     }
 }
 
-impl TemplateApp {
+impl App {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -28,29 +108,29 @@ impl TemplateApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
 
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
+impl eframe::App for App {
+    // /// Called by the frame work to save state before shutdown.
+    // fn save(&mut self, storage: &mut dyn eframe::Storage) {
+    //     eframe::set_value(storage, eframe::APP_KEY, self);
+    // }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        let Self {
+            removeThumb,
+            bodies,
+            hats,
+            ferris,
+        } = self;
 
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
@@ -64,44 +144,95 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+        egui::SidePanel::left("side_panel")
+            .resizable(false)
+            .min_width(200.0)
+            .show(ctx, |ui| {
+                ui.collapsing(RichText::new("Body").size(20.0), |ui| {
+                    egui::Grid::new("bodies").show(ui, |ui| {
+                        for (name, image) in bodies.iter() {
+                            if ui
+                                .add(egui::ImageButton::new(
+                                    image.thumb.texture_id(ctx),
+                                    image.thumb.size_vec2(),
+                                ))
+                                .clicked()
+                            {
+                                ferris.color = name.to_string();
+                            };
+                        }
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                        // crab.show(ui);
+                        // crab.show(ui);
+                        // ui.end_row();
+                        // crab.show(ui);
+                        // crab.show(ui);
+                    });
+                });
+
+                // ui.collapsing(RichText::new("Eyes").size(20.0), |ui|{
+                //     crab.show(ui);
+                // });
+
+                ui.collapsing(RichText::new("Hats").size(20.0), |ui|{
+                    egui::Grid::new("hats").show(ui, |ui| {
+                        if ui
+                        .add(egui::ImageButton::new(
+                            removeThumb.texture_id(ctx),
+                            removeThumb.size_vec2(),
+                        ))
+                        .clicked()
+                    {
+                        ferris.hat = None
+                    };
+
+                        for (name, image) in hats.iter() {
+                            if ui
+                                .add(egui::ImageButton::new(
+                                    image.thumb.texture_id(ctx),
+                                    image.thumb.size_vec2(),
+                                ))
+                                .clicked()
+                            {
+                                ferris.hat = Some(name.to_string());
+                            };
+                        }
+
+                        // crab.show(ui);
+                        // crab.show(ui);
+                        // ui.end_row();
+                        // crab.show(ui);
+                        // crab.show(ui);
+                    });
+                });
+
+                if ui.button("save").clicked() {
+                    ui.ctx().output_mut(|o| {
+                        o.open_url = Some(egui::output::OpenUrl {
+                            url: "https://github.com".to_string(),
+                            new_tab: true,
+                        });
+                    });
+                }
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
+        egui::Area::new("my_area").movable(false).show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                if let Some(image) = bodies.get(&ferris.color) {
+                    // TODO how to use show_scaled when window resizes???
+                    image.main.show(ui);
+                }
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+        egui::Area::new("my_area").movable(false).show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                if let Some(hat) = &ferris.hat {
+                    if let Some(image) = hats.get(hat) {
+                        image.main.show(ui);
+                    }
+                }
+            });
         });
 
         if false {
